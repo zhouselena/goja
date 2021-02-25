4c4324802a4f4782aa31b1da3a4d68f4acd3328f
tsedgwick <tim.sedgwick@10gen.com>
2021-02-25 15:33:27 -0600

    REALMC-8271: Handle non-object case toString (#18)

--- object.go
+++ object.go
@@ -868,6 +868,10 @@ func (o *Object) tryPrimitive(methodName unistring.String) Value {
 }
 
 func (o *Object) genericToPrimitiveNumber() Value {
+	if o.Prototype() == nil {
+		o.self.setProto(o.runtime.global.ObjectPrototype, false)
+	}
+
 	if v := o.tryPrimitive("valueOf"); v != nil {
 		return v
 	}
@@ -884,6 +888,10 @@ func (o *baseObject) toPrimitiveNumber() Value {
 }
 
 func (o *Object) genericToPrimitiveString() Value {
+	if o.Prototype() == nil {
+		o.self.setProto(o.runtime.global.ObjectPrototype, false)
+	}
+
 	if v := o.tryPrimitive("toString"); v != nil {
 		return v
 	}
